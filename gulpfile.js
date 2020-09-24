b20990d1b0dcb05e3520c1d4a6204f302b2ba3b9
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-09-24 16:51:37 +0500

    feat: add block headr

--- gulpfile.js
+++ gulpfile.js
@@ -149,13 +149,13 @@ function js() {
 
 function images() {
    return src(path.src.img)
-      .pipe(
-         webp({
-            quality: 70
-         })
-      )
-      .pipe(dest(path.build.img))
-      .pipe(src(path.src.img))
+      // .pipe(
+      //    webp({
+      //       quality: 70
+      //    })
+      // )
+      // .pipe(dest(path.build.img))
+      // .pipe(src(path.src.img))
       .pipe(
          imagemin({
             progressive: true,
